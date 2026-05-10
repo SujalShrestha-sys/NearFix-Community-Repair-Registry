91625ff2de67ceb6d5cdea3223181018ddfe9f6b
anshubajgain <bajgainanshu@gmail.com>
2026-05-10 09:22:57 +0545

    feat: implement HomeServlet to render landing page with dynamic statistics

--- src/main/java/nearfix/nearfix/controller/HomeServlet.java
+++ src/main/java/nearfix/nearfix/controller/HomeServlet.java
@@ -1,5 +1,6 @@
 package nearfix.nearfix.controller;
 
+// added comment
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
